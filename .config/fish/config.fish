e5e1ebe3617cc1513d7f1d8c53059609db5ce243
Marcos Alfredo Núñez <silver94@gmail.com>
2020-05-05 12:02:15 -0300

    add fish, compton

---
+++ .config/fish/config.fish
@@ -0,0 +1,4 @@
+alias dotfiles='/usr/bin/git --git-dir=/home/marcos/.dotfiles/ --work-tree=/home/marcos'
+set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish" 
+powerline-setup
+neofetch
